8b9f7279e9f2d0c8b8ac2286f97e2d42e471a2fb
Gonzalo174 <gonzalogiordano1@gmail.com>
2023-05-14 22:38:13 -0300

    Cambios: escribi cambios

--- PIV.functions.py
+++ PIV.functions.py
@@ -10,6 +10,8 @@ import imageio.v3 as iio
 from scipy import signal    # Para aplicar filtros
 from wand.image import Image
 
+# cambios
+
 #%%
 def centrar_referencia(imagen_post, imagen_pre, bordes_extra, maximo = False):
     l = imagen_pre.shape[0]
